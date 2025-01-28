b56e20ffce0b1a0a4612e9caed81b957c637a9f8
Tony Kovanen <tony.kovanen@gmail.com>
2025-01-27 16:07:18 -0800

    yes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
-import { mastra } from './mastra';
+import { mastra } from './mastra'
 
 const specieSchema = z.object({
   species: z.string(),
-});
+})
 
 const main = async () => {
-  const agentCat = mastra.getAgent('catOne');
+  const agentCat = mastra.getAgent('catOne')
 
   try {
-    const result = await agentCat.generate('What is the most popular cat species?', {
-      output: specieSchema,
-    });
+    const result = await agentCat.generate(
+      'What is the most popular cat species?',
+      {
+        output: specieSchema,
+      }
+    )
 
-    const res = specieSchema.parse(result?.object);
+    const res = specieSchema.parse(result?.object)
 
-    console.log(res.species);
+    console.log(res.species)
 
-    const { start } = mastra.getWorkflow('logCatWorkflow').createRun();
+    const { start } = mastra.getWorkflow('logCatWorkflow').createRun()
 
-    await start({ triggerData: { name: res.species } });
+    await start({ triggerData: { name: res.species } })
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
-main();
+main()
