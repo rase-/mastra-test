05a955b1fd0e569fa86f5ba736b637232f7d600c
Tony Kovanen <tony.kovanen@gmail.com>
2025-01-27 17:14:06 -0800

    update deps

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,10 +1,28 @@
-import { Mastra, createLogger } from '@mastra/core';
+import { Mastra, createLogger } from '@mastra/core'
+import { UpstashTransport } from '@mastra/loggers'
 
-import { catOne, agentTwo } from './agents/agent';
-import { logCatWorkflow } from './workflow';
+import { catOne, agentTwo } from './agents/agent'
+import { logCatWorkflow } from './workflow'
+
+const logger = createLogger({
+  name: 'MastraCloud',
+  transports: {
+    default: new UpstashTransport({
+      listName: 'production-logs',
+      upstashUrl: 'https://saved-lab-38332.upstash.io',
+      upstashToken:
+        'AZW8AAIjcDEyMjBmMjRiYzczMTI0ZWM4ODU5NThiODRjMGY0MTIzMnAxMA',
+    }),
+  },
+  overrideDefaultTransports: true,
+  level: 'debug',
+})
 
 export const mastra = new Mastra({
   agents: { catOne, agentTwo },
-  logger: false,
   workflows: { logCatWorkflow },
-});
+})
+
+setTimeout(() => {
+  mastra.setLogger({ logger })
+}, 1000)
