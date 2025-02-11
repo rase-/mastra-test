7ccf52571264e6aabb92fc468346868c601be5ba
Tony Kovanen <tony.kovanen@gmail.com>
2025-02-11 15:36:57 +0200

    update agents

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,16 +2,19 @@ import { Mastra, MastraStorageLibSql } from '@mastra/core'
 import { catOne, agentTwo } from './agents/agent'
 import { logCatWorkflow } from './workflow'
 
+const storage = new MastraStorageLibSql({
+  config: {
+    // url: 'file:mastra.db',
+    url: 'libsql://testdb-rase-.turso.io',
+    authToken:
+      'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzkyODA2NzIsImlkIjoiYjNlZjFkYjEtMGQ0My00NzdkLWFmNTEtZjdiYmFhMGY1YjU4In0.P_7fstavSxQv8IqoqOnGyCw-8xTP2caKri-itlmqWIaUsVKWqNOPnB55Z7mqaUUsysrDgr0Or8bjSf5AG6ewAQ',
+  },
+})
+
+// await storage.sync()
+
 export const mastra = new Mastra({
   agents: { catOne, agentTwo },
   workflows: { logCatWorkflow },
-  storage:
-    process.env.MASTRA_DB_URL && process.env.MASTRA_DB_AUTH_TOKEN
-      ? new MastraStorageLibSql({
-          config: {
-            url: process.env.MASTRA_DB_URL,
-            authToken: process.env.MASTRA_DB_AUTH_TOKEN,
-          },
-        })
-      : undefined,
+  storage,
 })
