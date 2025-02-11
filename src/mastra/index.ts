57bc75ded3b741286910f728e49d785f458bd640
Tony Kovanen <tony.kovanen@gmail.com>
2025-02-11 15:22:17 +0200

    optional built-in db

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,8 +1,17 @@
-import { Mastra } from '@mastra/core'
+import { Mastra, MastraStorageLibSql } from '@mastra/core'
 import { catOne, agentTwo } from './agents/agent'
 import { logCatWorkflow } from './workflow'
 
 export const mastra = new Mastra({
   agents: { catOne, agentTwo },
   workflows: { logCatWorkflow },
+  storage:
+    process.env.MASTRA_DB_URL && process.env.MASTRA_DB_AUTH_TOKEN
+      ? new MastraStorageLibSql({
+          config: {
+            url: process.env.MASTRA_DB_URL,
+            authToken: process.env.MASTRA_DB_AUTH_TOKEN,
+          },
+        })
+      : undefined,
 })
