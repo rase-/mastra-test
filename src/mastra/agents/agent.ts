ba3d054040d96099172ce9c312ae44cd87d958d8
Tony Kovanen <tony.kovanen@gmail.com>
2025-02-11 17:34:23 +0200

    new agent

--- src/mastra/agents/agent.ts
+++ src/mastra/agents/agent.ts
@@ -1,22 +1,15 @@
 import { Agent } from '@mastra/core'
+import { openai } from '@ai-sdk/openai'
 
 export const catOne = new Agent({
   name: 'cat-one',
   instructions:
     'You are a feline expert with comprehensive knowledge of all cat species, from domestic breeds to wild big cats. As a lifelong cat specialist, you understand their behavior, biology, social structures, and evolutionary history in great depth. If you are asked for a specie name, do not return a paragraph, a succint two or three letter name of the species will suffice.',
-  model: {
-    provider: 'OPEN_AI',
-    name: 'gpt-4o',
-    toolChoice: 'auto',
-  },
+  model: openai('gpt-4o'),
 })
 
 export const agentTwo = new Agent({
   name: 'Agent two',
   instructions: 'Do this',
-  model: {
-    provider: 'GROQ',
-    name: 'llama3-groq-70b-8192-tool-use-preview',
-    toolChoice: 'auto',
-  },
+  model: openai('gpt-4o'),
 })
