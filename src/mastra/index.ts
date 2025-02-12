6cfb587c1e4d132cf921254d0dc6423eeed0c9f5
Tony Kovanen <tony.kovanen@gmail.com>
2025-02-12 10:05:40 +0200

    remove storage from example

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,19 +2,7 @@ import { Mastra, MastraStorageLibSql } from '@mastra/core'
 import { catOne, agentTwo } from './agents/agent'
 import { logCatWorkflow } from './workflow'
 
-let storage = undefined
-if (process.env.MASTRA_STORAGE_URL && process.env.MASTRA_STORAGE_AUTH_TOKEN) {
-  console.log('Using Mastra Cloud Storage: ' + process.env.MASTRA_STORAGE_URL)
-  storage = new MastraStorageLibSql({
-    config: {
-      url: process.env.MASTRA_STORAGE_URL,
-      authToken: process.env.MASTRA_STORAGE_AUTH_TOKEN,
-    },
-  })
-}
-
 export const mastra = new Mastra({
   agents: { catOne, agentTwo },
   workflows: { logCatWorkflow },
-  storage,
 })
